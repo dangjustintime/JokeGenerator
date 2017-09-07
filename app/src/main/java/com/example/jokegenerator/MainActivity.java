3be8f653ae8a276528ee317f8eb44bd7e47d55d9
Justin Dang <dangjustintime@gmail.com>
2017-09-07 09:25:35 -0400

    currently working on making the background change colors everytime you open the app

--- app/src/main/java/com/example/jokegenerator/MainActivity.java
+++ app/src/main/java/com/example/jokegenerator/MainActivity.java
@@ -6,13 +6,37 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int randomNumber;
+    private double randomNumber;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        randomNumber = Math.random();
+        //change background color every time user opens app
+        if(randomNumber >= 0.0 && randomNumber < 0.1) {
+
+        } else if(randomNumber >= 0.1 && randomNumber < 0.2) {
+
+        } else if(randomNumber >= 0.2 && randomNumber < 0.3) {
+
+        } else if(randomNumber >= 0.3 && randomNumber < 0.4) {
+
+        } else if(randomNumber >= 0.4 && randomNumber < 0.5) {
+
+        } else if(randomNumber >= 0.5 && randomNumber < 0.6) {
+
+        } else if(randomNumber >= 0.6 && randomNumber < 0.7) {
+
+        } else if(randomNumber >= 0.7 && randomNumber < 0.8) {
+
+        } else if(randomNumber >= 0.8 && randomNumber < 0.9) {
+
+        } else if(randomNumber >= 0.9 && randomNumber < 1.0) {
+
+        }
 
     }
 }
