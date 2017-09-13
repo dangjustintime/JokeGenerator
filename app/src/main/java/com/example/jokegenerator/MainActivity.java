e14eb382126bbe876794af61eafaf5a140bed1ac
dangjustintime <dangjustintime@gmail.com>
2017-09-13 18:40:40 -0400

    added cursor object to query database

--- app/src/main/java/com/example/jokegenerator/MainActivity.java
+++ app/src/main/java/com/example/jokegenerator/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.jokegenerator;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    FeedReaderDbHelper mDbHelper;
+    SQLiteDatabase db;
+    String[] projection;
+    String selection;
+    String selectionArgs;
+    String sortOrder;
+    Cursor cursor;
+
     private double randomNumber;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         randomNumber = Math.random();
         //change background color every time user opens app
+        /*
         if(randomNumber >= 0.0 && randomNumber < 0.1) {
 
         } else if(randomNumber >= 0.1 && randomNumber < 0.2) {
@@ -37,6 +46,29 @@ public class MainActivity extends AppCompatActivity {
         } else if(randomNumber >= 0.9 && randomNumber < 1.0) {
 
         }
+        */
+
+        //instantiate FeedReaderDbHelper
+        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(getApplicationContext());
+
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        //specifiy which columns from the database you are using
+        String[] projection =  {
+                FeedReaderContract.FeedEntry.COLUMN_SETUP,
+                FeedReaderContract.FeedEntry.COLUMN_PUNCHLINE
+        };
+
+        Cursor cursor = db.query(
+                FeedReaderContract.FeedEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
 
     }
 }
