c68c8a46c4ab3ff26912433f329b04698774e582
Justin Dang <dangjustintime@gmail.com>
2017-09-15 12:01:43 -0400

    moved database management to joke activity. Moved changing background color into its own method

--- app/src/main/java/com/example/jokegenerator/JokeActivity.java
+++ app/src/main/java/com/example/jokegenerator/JokeActivity.java
@@ -1,13 +1,64 @@
 package com.example.jokegenerator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
+
+    TextView jokeTextView;
+
+    FeedReaderDbHelper mDbHelper;
+    SQLiteDatabase db;
+    String[] projection;
+    String selection;
+    String selectionArgs;
+    String sortOrder;
+    Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
+
+        jokeTextView = (TextView) findViewById(R.id.text_view_joke);
+        jokeTextView.setText("Why did the chicken cross the road?");
+
+
+
+        jokeTextView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                jokeTextView.setText("To get to the other side.");
+            }
+        });
+
+        /*
+        //instantiate FeedReaderDbHelper
+        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(getApplicationContext());
+
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        //specifiy which columns from the database you are using
+        String[] projection =  {
+                FeedReaderContract.FeedEntry.COLUMN_SETUP,
+                FeedReaderContract.FeedEntry.COLUMN_PUNCHLINE
+        };
+        */
+        /*
+        Cursor cursor = db.query(
+                FeedReaderContract.FeedEntry.TABLE_NAME,    //table name
+                projection,     //selected columns to display
+                null,       //selected rows, null == show all rows
+                null,       //order of rows displayed
+                null,       //row groups
+                null,       //which row groups to include, null == show all row groups
+                null        //sort order
+        );
+        */
+
+
     }
 }
--- app/src/main/java/com/example/jokegenerator/MainActivity.java
+++ app/src/main/java/com/example/jokegenerator/MainActivity.java
@@ -1,76 +1,62 @@
 package com.example.jokegenerator;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    FeedReaderDbHelper mDbHelper;
-    SQLiteDatabase db;
-    String[] projection;
-    String selection;
-    String selectionArgs;
-    String sortOrder;
-    Cursor cursor;
+    private ImageView funnyFaceEmojiImageView;
+    private LinearLayout mainActivityLinearLayout;
 
-    private double randomNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        randomNumber = Math.random();
-        //change background color every time user opens app
-        /*
-        if(randomNumber >= 0.0 && randomNumber < 0.1) {
 
-        } else if(randomNumber >= 0.1 && randomNumber < 0.2) {
+        //mainActivityLinearLayout = (LinearLayout) findViewById(R.id.linear_layout_main_activity);
+        funnyFaceEmojiImageView = (ImageView) findViewById(R.id.image_view_funny_face_emoji);
 
-        } else if(randomNumber >= 0.2 && randomNumber < 0.3) {
-
-        } else if(randomNumber >= 0.3 && randomNumber < 0.4) {
-
-        } else if(randomNumber >= 0.4 && randomNumber < 0.5) {
-
-        } else if(randomNumber >= 0.5 && randomNumber < 0.6) {
-
-        } else if(randomNumber >= 0.6 && randomNumber < 0.7) {
-
-        } else if(randomNumber >= 0.7 && randomNumber < 0.8) {
-
-        } else if(randomNumber >= 0.8 && randomNumber < 0.9) {
-
-        } else if(randomNumber >= 0.9 && randomNumber < 1.0) {
-
-        }
-        */
-
-        //instantiate FeedReaderDbHelper
-        //FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(getApplicationContext());
-
-        //SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        //specifiy which columns from the database you are using
-        String[] projection =  {
-                FeedReaderContract.FeedEntry.COLUMN_SETUP,
-                FeedReaderContract.FeedEntry.COLUMN_PUNCHLINE
-        };
-
-        /*
-        Cursor cursor = db.query(
-                FeedReaderContract.FeedEntry.TABLE_NAME,    //table name
-                projection,     //selected columns to display
-                null,       //selected rows, null == show all rows
-                null,       //order of rows displayed
-                null,       //row groups
-                null,       //which row groups to include, null == show all row groups
-                null        //sort order
-        );
-        */
-
+        funnyFaceEmojiImageView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, JokeActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
+
+/*
+public void ChangeBackgroundColor(View v) {
+    double randomNumber;
+    randomNumber = Math.random();
+    //change background color every time user opens app
+    if(randomNumber >= 0.0 && randomNumber < 0.1) {
+        v.setBackgroundColor(getResources().getColor(R.color.blue));
+    } else if(randomNumber >= 0.1 && randomNumber < 0.2) {
+        v.setBackgroundColor(getResources().getColor(R.color.cyan));
+    } else if(randomNumber >= 0.2 && randomNumber < 0.3) {
+        v.setBackgroundColor(getResources().getColor(R.color.deepPurple));
+    } else if(randomNumber >= 0.3 && randomNumber < 0.4) {
+        v.setBackgroundColor(getResources().getColor(R.color.green));
+    } else if(randomNumber >= 0.4 && randomNumber < 0.5) {
+        v.setBackgroundColor(getResources().getColor(R.color.lightBlue));
+    } else if(randomNumber >= 0.5 && randomNumber < 0.6) {
+        v.setBackgroundColor(getResources().getColor(R.color.orange));
+    } else if(randomNumber >= 0.6 && randomNumber < 0.7) {
+        v.setBackgroundColor(getResources().getColor(R.color.pink));
+    } else if(randomNumber >= 0.7 && randomNumber < 0.8) {
+        v.setBackgroundColor(getResources().getColor(R.color.purple));
+    } else if(randomNumber >= 0.8 && randomNumber < 0.9) {
+        v.setBackgroundColor(getResources().getColor(R.color.red));
+    } else if(randomNumber >= 0.9 && randomNumber < 1.0) {
+        v.setBackgroundColor(getResources().getColor(R.color.yellow));
+    }
+}
+*/
